30a17caeb399997f0f7306420ea96f7b677a9f71
Ivo Araujo <ivo.souza-23@outlook.com>
2024-07-17 11:52:55 -0300

    closes #04 - Implementar as 4 operações matemáticas

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-function calc(){
+function calc(operador){
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
 
-    var output = num1 + num2;
-
-    document.getElementById("output").value = output;
+    switch(operador){
+        case '+':
+            var somaoutput = num1 + num2;
+            document.getElementById("output").value = somaoutput;
+        break;
+        case '-':
+            var suboutput = num1 - num2;
+            document.getElementById("output").value = suboutput;
+        break;
+        case '*':
+            var multoutput = num1 * num2;
+            document.getElementById("output").value = multoutput;
+        break;
+        case '/':
+            if(num2 === 0){
+                alert("Não é possível dividir por zero");
+            }else{
+                var dividiroutput = num1 / num2;
+                document.getElementById("output").value = dividiroutput;
+            }
+        break;
+        default:
+            alert("Operador inválido");
+    }
+    //document.getElementById("output").value = output;
 }
